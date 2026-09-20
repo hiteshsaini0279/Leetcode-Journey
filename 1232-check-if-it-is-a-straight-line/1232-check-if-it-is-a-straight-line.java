53015306ccc29e27c01376a8f8427e013beaaef9
Hitesh Saini <hiteshsaini0277@gmail.com>
2026-09-20 12:54:28 +0530

    Time: 0 ms (100%), Space: 45.1 MB (64.59%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        for(int i =1; i+1<coordinates.length; i++){
+             if(coordinates[i-1][0]*(coordinates[i][1]-coordinates[i+1][1])+coordinates[i][0]*(coordinates[i+1][1]-coordinates[i-1][1])+coordinates[i+1][0]*(coordinates[i-1][1]-coordinates[i][1])!=0){
+    return false;
+  }
+
+        }
+        return true;
+    }
+}
