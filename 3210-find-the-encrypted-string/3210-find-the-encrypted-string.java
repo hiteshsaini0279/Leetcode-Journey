583d8b1b096452447725215cc9cd9dbb7e7a50bc
Hitesh Saini <hiteshsaini0277@gmail.com>
2026-09-22 16:47:14 +0530

    Time: 4 ms (14.94%), Space: 45 MB (6.51%) - LeetHub

---
+++ 3210-find-the-encrypted-string/3210-find-the-encrypted-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String getEncryptedString(String s, int k) {
+        int n=s.length();
+        String ans="";
+
+        for(int i=0; i<n; i++){
+            ans+=s.charAt((i+k)%n);
+        }
+        return ans;
+    }
+}
